e663c435b81292c9aa7552e84f6408b5d3b43120
Antonio Carlos Javier Cuello <67564308+javicuello9@users.noreply.github.com>
2020-06-29 18:28:44 -0300

    First Commmit

---
+++ Module1.vb
@@ -0,0 +1,51 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Kg As Double
+        Dim PrecioPorKg As Double
+        Dim Calidad As String
+        Dim tamaño As Int32
+
+        Console.WriteLine("Ingrese Calidad siendo : A  ó B .")
+        Calidad = Console.ReadLine.ToUpper
+
+
+        Select Case Calidad
+            Case "A"
+
+                Console.WriteLine("Ingreste Tamaño: 1 o 2")
+                tamaño = Console.ReadLine
+                Console.WriteLine("Ingrese precio por Kg de  Uva.")
+                PrecioPorKg = Console.ReadLine
+                Console.WriteLine("Ingrese Kg de  Uva.")
+                Kg = Console.ReadLine
+
+                If tamaño = 1 Then
+                    Console.WriteLine("Usted obtiene de ganacia: $" & (PrecioPorKg * Kg) + 20)
+                Else
+                    Console.WriteLine("Usted obtiene de ganacia: $" & (PrecioPorKg * Kg) + 30)
+
+                End If
+
+            Case "B"
+
+                Console.WriteLine("Ingreste Tamaño: 1 o 2")
+                tamaño = Console.ReadLine
+                Console.WriteLine("Ingrese precio por Kg de  Uva.")
+                PrecioPorKg = Console.ReadLine
+                Console.WriteLine("Ingrese Kg de  Uva.")
+                Kg = Console.ReadLine
+
+                If tamaño = 1 Then
+                    Console.WriteLine("Usted obtiene de ganacia: $" & (PrecioPorKg * Kg) - 30)
+                Else
+                    Console.WriteLine("Usted obtiene de ganacia: $" & (PrecioPorKg * Kg) - 50)
+
+                End If
+            Case Else
+                Console.WriteLine("ERORR..!! Ingrese Letra A ó B")
+        End Select
+        Console.Read()
+    End Sub
+
+End Module
